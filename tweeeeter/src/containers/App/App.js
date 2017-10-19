d3ae756e13abe48d3f8794164653e2c4c3572b6e
Pruthvi UVSS <pruthviuvss@gmail.com>
2017-10-19 00:56:35 -0400

    Add files via upload

--- tweeeeter/src/containers/App/App.js
+++ tweeeeter/src/containers/App/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from '../../logo.svg';
-
+import React, {Component} from 'react';
 import Home from '../../containers/Home/Home'
 import Connect from '../../containers/Connect/Connect'
-var background={
-    backgroundColor:'#0084b4',
-    minWidth: '100%',
-    minHeight: '100%'
+import Header from '../Header/Header'
 
-}
 class App extends Component {
-  render() {
-      return (
-          <div className="App" >
-              <header>
-                  <h1>Tweeeeter</h1>
-                  <h3>Now tweet Longer</h3>
-              </header>
-              <Home/>
-              <Connect/>
+    render() {
+        return (
+            <div className="body">
+                <Header/>
+                <div id="bodyContainer">
+                    <Home/>
+                    <Connect/>
+                </div>
 
-          </div>
-      );
-  }
+            </div>
+
+        );
+    }
 }
 
 export default App;
