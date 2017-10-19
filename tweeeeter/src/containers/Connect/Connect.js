c70b5997447e36b93d90455b4347dc5c48781ea7
Pruthvi UVSS <pruthviuvss@gmail.com>
2017-10-19 00:55:33 -0400

    Add files via upload

--- tweeeeter/src/containers/Connect/Connect.js
+++ tweeeeter/src/containers/Connect/Connect.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from '../../logo.svg';
 import '../../App.css';
-var divStyle={
-    position: 'fixed',
-    marginLeft:'60%',
-    height: '70%',
-width: '20%',
-backgroundColor: 'powderblue'
 
-}
+
 class Connect extends Component {
     render() {
         return (
-            <div style={divStyle} className="w3-card-4">
-                    Sign in with Twitter.
+            <div id="contentContainer" className="w3-card-4">
+                <textarea id="textAreaBox" rows="4" cols="76"></textarea>
             </div>
         );
     }
